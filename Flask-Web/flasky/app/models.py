a435df1c4432321033b2e203671e25b75b6263bb
HC <fzhuse@gmail.com>
2020-09-25 22:07:50 +0200

    v1.2

--- Flask-Web/flasky/app/models.py
+++ Flask-Web/flasky/app/models.py
@@ -1,11 +1,15 @@
 # 必须从app中导入db，确保这个db是初始化了app的db，这样才能保证模型与app相关联而不能在本模块中再创建新的db，
 # 因为这将创建新的SQLAlchemy实例
+
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
+import hashlib
+import bleach
+from markdown import markdown
 
 
 class Permission:
@@ -25,7 +29,7 @@ class Role(db.Model):
     permissions = db.Column(db.Integer)
 
     def __init__(self, **kwargs):
-        super(Role, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         if self.permissions is None:
             self.permissions = 0
 
@@ -75,22 +79,43 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)  # 用于记录第一次登录时间
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)  # 用于记录最后一次登录时间
+    avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')  # 给Post一个反向关联属性author
 
     def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         if self.role is None:  # 当新注册的用户还没有对应于Role的role反向属性的时候
             if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
 
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    # 计算邮箱散列值，避免每次渲染头像都计算一次散列值
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    # 定义头像加密URL，头像关联用户email
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or self.gravatar_hash()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -159,13 +184,13 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:  # 检查新地址是否等于原地址
             return False
         self.email = new_email
+        self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
         return True
 
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
-        db.commit()
 
 
 class AnonymousUser(AnonymousUserMixin):
@@ -179,6 +204,28 @@ class AnonymousUser(AnonymousUserMixin):
 login_manager.anonymous_user = AnonymousUser
 
 
-@login_manager.user_loader
+@login_manager.user_loader  # 每次生成请求上下文的时候生成current_user
 def load_user(user_id):
     return User.query.get(int(user_id))  # get() 返回指定主键对应的行，如果没有对应的行，则返回None
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, old_value, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+                                                       tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
+
--- Flask-Web/flasky/config.py
+++ Flask-Web/flasky/config.py
@@ -5,10 +5,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'THIS IS A SECRET KEY'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+                   ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    # 使用gmail发送邮件会被google服务器阻拦
     FLASKY_MAIL_SENDER = 'Flasky Admin <fzhuse@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'fzhuse@gmail.com'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
@@ -17,11 +26,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
@@ -34,7 +38,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data-pdt.sqlite')
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
